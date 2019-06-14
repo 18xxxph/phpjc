b845b9a7785c7f59ae2b08a4e7c49d3865f52828
18xxxph <49505197+18xxxph@users.noreply.github.com>
2019-06-14 09:07:20 +0800

    Add files via upload

---
+++ 16.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>面向对象</title>
+</head>
+
+<?php
+class Site {
+  /* 成员变量 */
+  var $url;
+  var $title;
+  
+  /* 成员函数 */
+  function setUrl($par){
+     $this->url = $par;
+  }
+  
+  function getUrl(){
+     echo $this->url . PHP_EOL;
+  }
+  
+  function setTitle($par){
+     $this->title = $par;
+  }
+  
+  function getTitle(){
+     echo $this->title . PHP_EOL;
+  }
+}
+?>
+
+
+
+
+
+
+<body>
+</body>
+</html>
